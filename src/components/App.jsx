7645d7c03e6fc3f854c049e516440d83d1b6db0e
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-09-10 20:32:02 +0200

    init commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Products from '../pages/Products';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
