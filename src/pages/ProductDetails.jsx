4ed9bf5a1afa6ee817c7eb06fdd984347a719da8
Madina Konurbaeva <madina.konurbaeva@mcmakler.de>
2022-09-11 13:51:08 +0200

    WIP

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -2,14 +2,17 @@ import { useParams } from 'react-router-dom';
 import { getProductById } from '../fakeAPI';
 
 const ProductDetails = () => {
-  const { id } = useParams;
+  const { id } = useParams();
   const product = getProductById(id);
+  console.log('product: ', product);
 
   return (
     <main>
       <img src="https://via.placeholder.com/960x240" alt="" />
       <div>
-        <h2>Product {product.name}</h2>
+        <h2>
+          Product {product.name}- {id}
+        </h2>
       </div>
     </main>
   );
